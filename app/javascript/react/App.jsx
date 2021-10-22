c77e9356ee78afb17b1c9e3e46b0f6ba0d0c53e7
Dipanjan Panja <dipanjanpanja6@gmail.com>
2021-10-23 00:51:57 +0530

    fix: #2

--- app/javascript/react/App.jsx
+++ app/javascript/react/App.jsx
@@ -1,20 +1,16 @@
 import React from "react"
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 
 import Products from "./views/products"
 
-const App = () => {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" exact component={Products} />
-          <Route path="/products" exact component={Products} />
-        </Switch>
-      </Router>
-    </>
-  )
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" exact render={() => <Redirect to="/products" />} />
+      <Route path="/products" exact component={Products} />
+    </Switch>
+  </Router>
+)
 
 export default App
